ea1af0828b792826a8dd711733c70fbc0110707f
Raul <raul@macarie.me>
2021-04-02 13:42:58 +0200

    Group entries by their _clean_ section name

--- src/helpers/create-special-section.ts
+++ src/helpers/create-special-section.ts
@@ -1,5 +1,4 @@
 import fixSpecialFeaturedName from './fix-specal-featured-name'
-import sortEntries from './sort-entries'
 import createFeaturedSection from './create-featured-section'
 
 import type ShopEntryType from '../types/shop-entry'
@@ -9,10 +8,28 @@ export const createSpecialSection = (
 ): Array<{
   sectionName: string
   sectionEntries: Array<Array<Array<ShopEntryType<string>>>>
-}> =>
-  createFeaturedSection(specialItems).map((entries) => ({
-    sectionName: fixSpecialFeaturedName(entries[0][0].sectionId),
-    sectionEntries: [entries.map((entry) => sortEntries(entry))],
+}> => {
+  const featuredSections = createFeaturedSection(specialItems)
+
+  const groupedSections = new Map<
+    string,
+    Array<Array<Array<ShopEntryType<string>>>>
+  >()
+
+  for (const featuredSection of featuredSections) {
+    const sectionName = fixSpecialFeaturedName(featuredSection[0][0].sectionId)
+
+    if (groupedSections.has(sectionName)) {
+      groupedSections.get(sectionName)!.push(featuredSection)
+    } else {
+      groupedSections.set(sectionName, [featuredSection])
+    }
+  }
+
+  return [...groupedSections.entries()].map(([name, sections]) => ({
+    sectionName: name,
+    sectionEntries: sections,
   }))
+}
 
 export default createSpecialSection
