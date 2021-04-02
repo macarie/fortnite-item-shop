98a26292bdc17503de0e71f4513f7290d711a2d8
Raul <raul@macarie.me>
2021-04-02 13:44:38 +0200

    Always un-PascalCase section names

--- src/helpers/fix-specal-featured-name.ts
+++ src/helpers/fix-specal-featured-name.ts
@@ -1,16 +1,12 @@
 export const fixSpecialFeaturedName = (name: string) => {
-  if (name.endsWith('B')) {
-    return name
-      .replace(/B$/, '')
-      .replace(/([A-Z\d])/g, ' $1')
-      .trim()
-  }
-
   if (name.startsWith('Special')) {
     return 'Special'
   }
 
   return name
+    .replace(/B$/, '')
+    .replace(/([A-Z\d])/g, ' $1')
+    .trim()
 }
 
 export default fixSpecialFeaturedName
